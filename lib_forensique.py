fd9f90182d1153249506604579b6fde42710103f
Nico <nicolas.schmitt@isen.yncrea.fr>
2020-03-26 19:48:08 +0100

    Ajout pcap nmap3 + ajout detectUdpPortScan() + modif detectTcpPortScan()
    nmap3.pcap comporte une capture de 2 scans de ports en utilisant le protocole UDP via nmap -sU ip_target
    A savoir que le scan UDP n'est pas très efficace en effet la machine cible renvoie une trame ICMP dans le cas d'un port fermé et ne renvoie rien si le port est ouvert, cependant un port peut être filtré et ne rien envoyer non plus

--- lib_forensique.py
+++ lib_forensique.py
@@ -29,23 +29,81 @@ def detectTcpPortScan(path):
     # Sort all ports sets for each (ip_origin, ip_target), sorted function return a sorted list
     for k in scan_report:
         for i in range(3):
-            scan_report[k][i] = sorted(scan_report[k][i]) # sets become lists
+            scan_report[k][i] = sorted(scan_report[k][i]) # Sets become list
     
     # Display the scan report at the screen
     if scan_report:
-        print('\n'+30*'-'+' PORTS SCAN DETECTED '+30*'-')
+        print('\n'+30*'-'+' TCP PORTS SCAN DETECTED '+30*'-')
 
         for (ip_origin, ip_target) in scan_report:
             scanned_ports = scan_report[(ip_origin, ip_target)][0]
             closed_ports = scan_report[(ip_origin, ip_target)][1]
             opened_ports = scan_report[(ip_origin, ip_target)][2]
-            nb_filtered_ports = len(scanned_ports) - len(closed_ports) - len(opened_ports)
-            print('Scan of {} ports (SYN flag sended by TCP) to {} from {}'.format(len(scanned_ports), ip_target, ip_origin))
-            print('{} port(s) filtered (No reply from {})'.format(nb_filtered_ports, ip_target))
+            filtered_ports = sorted(set(scanned_ports).difference(set(closed_ports).union(set(opened_ports))))
+
+            print('\nScan of {} ports (SYN flag sended by TCP) to {} from {}'.format(len(scanned_ports), ip_target, ip_origin))
+            print('{} port(s) filtered (No reply from {})'.format(len(filtered_ports), ip_target))
             print('{} port(s) closed (RST, ACK flags)'.format(len(closed_ports)))
-            print('{} port(s) opened (SYN ACK flags): {}\n'.format(len(opened_ports), " ".join([str(i) for i in opened_ports])))
+            if 0 < len(closed_ports) <= 20:
+                print(' '.join([str(i) for i in closed_ports]))
+            print('{} port(s) opened (SYN ACK flags)'.format(len(opened_ports)))
+            if 0 < len(opened_ports) <= 20:
+                print(' '.join([str(i) for i in opened_ports]))
 
     else:
-        print('\n'+30*'-'+'NO PORTS SCAN DETECTED '+30*'-')
+        print('\n'+30*'-'+'NO TCP PORTS SCAN DETECTED '+30*'-')
+
+    return scan_report
+
+
+def detectUdpPortScan(path):
+    pcap_file = rdpcap(path)
+    scan_report = dict()  # scan_report has this shape : {(ip_origin1, ip_traget1): [scanned_ports1,closed_ports1,opened_ports1], (ip_origin2, ip_traget2): [scanned_ports2,closed_ports2,opened_ports2], ..., (ip_originN, ip_tragetN): [scanned_portsN,closed_portsN,opened_portsN]}
+
+    # Read all frames of the pcap file
+    for frame in pcap_file:
+        layers = frame.layers()
+
+        if len(layers) > 2 and layers[2].__name__ == 'UDP':
+            ip_src = frame[IP].src
+            ip_dst = frame[IP].dst
+            port_dst = frame[UDP].dport
+
+            if (ip_src, ip_dst) not in scan_report:
+                scan_report.setdefault((ip_src, ip_dst), [set(),set(),set()])
+            scan_report[(ip_src, ip_dst)][0].add(port_dst)
+
+        elif len(layers) > 2 and layers[2].__name__ == 'ICMP':
+            ip_src = frame[IP].src
+            ip_dst = frame[IP].dst
+
+            if (scapy.layers.inet.UDPerror in layers):
+                port_dst = frame[UDPerror].dport
+                scan_report[(ip_dst, ip_src)][1].add(port_dst)
+
+    # Sort all ports sets for each (ip_origin, ip_target), sorted function return a sorted list
+    for k in scan_report:
+        for i in range(3):
+            scan_report[k][i] = sorted(scan_report[k][i]) # Sets become list
+    
+    # Display the scan report at the screen
+    if scan_report:
+        print('\n'+30*'-'+' UDP PORTS SCAN DETECTED '+30*'-')
+
+        for (ip_origin, ip_target) in scan_report:
+            scanned_ports = scan_report[(ip_origin, ip_target)][0]
+            closed_ports = scan_report[(ip_origin, ip_target)][1]
+            op_fil_ports = sorted(set(scanned_ports).difference(set(closed_ports)))
+
+            print('\nScan of {} ports to {} from {}'.format(len(scanned_ports), ip_target, ip_origin))
+            print('{} port(s) closed (ICMP answer)'.format(len(closed_ports)))
+            if 0 < len(closed_ports) <= 20:
+                print(' '.join([str(i) for i in closed_ports]))
+            print('{} port(s) opened | filtered (No answer)'.format(len(op_fil_ports)))
+            if 0 < len(op_fil_ports) <= 20:
+                print(' '.join([str(i) for i in op_fil_ports]))
+
+    else:
+        print('\n'+30*'-'+'NO UDP PORTS SCAN DETECTED '+30*'-')
 
     return scan_report
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import lib_forensique
 
 def main():
-    lib_forensique.detectTcpPortScan(path='Pcaps/nmap2.pcap')
+    #lib_forensique.detectTcpPortScan(path='Pcaps/nmap2.pcap') # Test the detection of TCP port scan
+    lib_forensique.detectUdpPortScan(path='Pcaps/nmap3.pcap') # Test the detection of UDP port scan
 
 main()
