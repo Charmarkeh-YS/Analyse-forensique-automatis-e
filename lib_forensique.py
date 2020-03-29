e71ba199c751ffb42175d625c3cd799e0e11a3e0
Nico <nicolas.schmitt@isen.yncrea.fr>
2020-03-29 19:54:03 +0200

    Ajout detectTcpPortScan2() + fichiers pcap nmap

--- lib_forensique.py
+++ lib_forensique.py
@@ -56,7 +56,7 @@ def detectTcpPortScan(path):
     return scan_report
 
 
-def detectUdpPortScan(path):
+def detectTcpPortScan2(path):
     pcap_file = rdpcap(path)
     scan_report = dict()  # scan_report has this shape : {(ip_origin1, ip_traget1): [scanned_ports1,closed_ports1,opened_ports1], (ip_origin2, ip_traget2): [scanned_ports2,closed_ports2,opened_ports2], ..., (ip_originN, ip_tragetN): [scanned_portsN,closed_portsN,opened_portsN]}
 
@@ -64,6 +64,59 @@ def detectUdpPortScan(path):
     for frame in pcap_file:
         layers = frame.layers()
 
+        if len(layers) > 2 and layers[2].__name__ == 'TCP':
+            ip_src = frame[IP].src
+            ip_dst = frame[IP].dst
+            port_src = frame[TCP].sport
+            port_dst = frame[TCP].dport
+
+            # FIN-PSH-URG-NULL flags
+            if frame[TCP].flags.value in [0x00, 0x01, 0x29]: # [NULL, FIN, FIN-PSH-URG]
+                if (ip_src, ip_dst) not in scan_report:
+                    scan_report.setdefault((ip_src, ip_dst), [set(),set(),set()])
+                scan_report[(ip_src, ip_dst)][0].add(port_dst)
+            # RST ACK flags
+            elif frame[TCP].flags.value == 0x14 and (ip_dst, ip_src) in scan_report:
+                scan_report[(ip_dst, ip_src)][1].add(port_src)
+
+    # Sort all ports sets for each (ip_origin, ip_target), sorted function return a sorted list
+    for k in scan_report:
+        for i in range(3):
+            scan_report[k][i] = sorted(scan_report[k][i]) # Sets become list
+    
+    # Display the scan report at the screen
+    if scan_report:
+        print('\n'+30*'-'+' INVERSE TCP PORTS SCAN DETECTED '+30*'-')
+
+        for (ip_origin, ip_target) in scan_report:
+            scanned_ports = scan_report[(ip_origin, ip_target)][0]
+            closed_ports = scan_report[(ip_origin, ip_target)][1]
+            op_fil_ports = sorted(set(scanned_ports).difference(set(closed_ports)))
+            scan_report[(ip_origin, ip_target)][2] = op_fil_ports
+
+            print('\nScan of {} ports (FIN-PUSH-URG-NULL flag sended by TCP) to {} from {}'.format(len(scanned_ports), ip_target, ip_origin))
+            print('{} port(s) closed (RST, ACK flags)'.format(len(closed_ports)))
+            if 0 < len(closed_ports) <= 20:
+                print(' '.join([str(i) for i in closed_ports]))
+            print('{} port(s) opened | filtered (No answer)'.format(len(op_fil_ports)))
+            if 0 < len(op_fil_ports) <= 20:
+                print(' '.join([str(i) for i in op_fil_ports]))
+
+    else:
+        print('\n'+30*'-'+'NO INVERSE TCP PORTS SCAN DETECTED '+30*'-')
+
+    return scan_report
+
+
+def detectUdpPortScan(path):
+    pcap_file = rdpcap(path)
+    scan_report = dict()  # scan_report has this shape : {(ip_origin1, ip_traget1): [scanned_ports1,closed_ports1,opened_filtered_ports1], (ip_origin2, ip_traget2): [scanned_ports2,closed_ports2,opened_filtered_ports2], ..., (ip_originN, ip_tragetN): [scanned_portsN,closed_portsN,opened_filtered_portsN]}
+
+    # Read all frames of the pcap file
+    for frame in pcap_file:
+        layers = frame.layers()
+
+        # Frame sent by the attacker
         if len(layers) > 2 and layers[2].__name__ == 'UDP':
             ip_src = frame[IP].src
             ip_dst = frame[IP].dst
@@ -73,6 +126,7 @@ def detectUdpPortScan(path):
                 scan_report.setdefault((ip_src, ip_dst), [set(),set(),set()])
             scan_report[(ip_src, ip_dst)][0].add(port_dst)
 
+        # Frame sent by the target in the case of closed port
         elif len(layers) > 2 and layers[2].__name__ == 'ICMP':
             ip_src = frame[IP].src
             ip_dst = frame[IP].dst
@@ -94,6 +148,7 @@ def detectUdpPortScan(path):
             scanned_ports = scan_report[(ip_origin, ip_target)][0]
             closed_ports = scan_report[(ip_origin, ip_target)][1]
             op_fil_ports = sorted(set(scanned_ports).difference(set(closed_ports)))
+            scan_report[(ip_origin, ip_target)][2] = op_fil_ports
 
             print('\nScan of {} ports to {} from {}'.format(len(scanned_ports), ip_target, ip_origin))
             print('{} port(s) closed (ICMP answer)'.format(len(closed_ports)))
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import lib_forensique
 
 def main():
     #lib_forensique.detectTcpPortScan(path='Pcaps/nmap2.pcap') # Test the detection of TCP port scan
-    lib_forensique.detectUdpPortScan(path='Pcaps/nmap3.pcap') # Test the detection of UDP port scan
+    #lib_forensique.detectUdpPortScan(path='Pcaps/nmap3.pcap') # Test the detection of UDP port scan
+    lib_forensique.detectTcpPortScan2(path='Pcaps/nmap_sX.pcap') # Test the detection of Inverse TCP port scan
 
 main()
