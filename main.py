5d7d1d0e00a42dfde1c7ac0562b488af98200aa8
H <raphael.huon@isen.yncrea.fr>
2020-04-08 11:54:13 +0200

    Dernier push comportait des erreurs : rectification

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     #lib_forensique.detectUdpPortScan(path='Pcaps/nmap3.pcap') # Test the detection of UDP port scan
     #lib_forensique.detectTcpPortScan2(path='Pcaps/nmap_sX.pcap') # Test the detection of Inverse TCP port scan
     #lib_forensique.detectNetworkArpScan(path='Pcaps/netdiscover.pcap') # Test the detection of network ARP scan
-    lib_forensique.detectTcpFlood(path='Pcaps/synflood.pcap') # Test the detection of TCP SYN flood
+    lib_forensique.detectTcpFlood(path='Pcaps/arppoison.pcap') # Test the detection of TCP SYN flood
 
     #handler = NetworkAnalyzer("./Pcaps/synflood.pcap")
     #handler.detectTcpPortScan()
